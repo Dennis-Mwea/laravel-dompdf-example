5e79770042bf05c2bd675fcaf30fc3802d1b7687
Dennis-Mwea <Mweadennis2@gmail.com>
2019-02-01 16:27:08 +0300

    Installation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ _('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form action="" method="post">
+                        @csrf
+
+                        <div class="form-roup row">
+                            <label for="fname" class="col-md-4 col-form-label text-md-right">{{ _('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="fname" type="text" class="form-control{{ $errors->has('fname') ? ' is-invalid' : '' }}" name="fname" value="{{ old('fname') }}" required autofocus>
+
+                                @if ($errors->has('fname'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('fname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
